7168eb1ebd98b20f507d82583e150c43bc6297aa
anitaaguilar003 <106880377+anitaaguilar003@users.noreply.github.com>
2022-07-09 17:05:28 -0500

    Add files via upload

---
+++ Actividad01/c3act01/Revista.java
@@ -0,0 +1,26 @@
+package com.cice.ejercicios.ejercicio3.logica;
+
+public class Revista extends Publicacion {
+
+    private int numero;
+
+    public Revista() {
+        super();
+    }
+
+    public Revista(int codigo, String titulo, int anyo, int numero) {
+        super(codigo,titulo,anyo);
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    @Override
+    public String toString () {
+        return super.toString()+
+                "\n\tNúmero: "+getNumero();
+    }
+
+}
